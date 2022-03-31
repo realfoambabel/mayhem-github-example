ba8266a3ac7a9930faca38f53054790b67bce472
David Brumley <dbrumley@forallsecure.com>
2022-03-31 18:45:36 -0400

    simplified example

--- myapp.c
+++ myapp.c
@@ -4,27 +4,15 @@
 
 int checkme(char *buf) {
   if(buf[0] == 'b' && buf[1] == 'u' && buf[2] == 'g'){
-    abort(); // introduced bug
+    abort(); // bug triggered!
     return 1;
   }
   return 0;
 }
 
 int main(int argc, char *argv[]) {
-  FILE *f;
   char buf[256];
 
-  if(argc != 2){
-    fprintf(stderr, "Must supply a text file\n");
-    return -1;
-  }
-  if( (f = fopen(argv[1], "r")) == NULL) {
-    fprintf(stderr, "Could not open %s\n", argv[1]);
-    return -1;
-  }
-  if(fgets(buf, sizeof(buf), f) == NULL){
-    fprintf(stderr, "Could not read from %s\n", argv[1]);
-    return -1;
-  }
+  read(0, buf, sizeof(buf));
   return checkme(buf);
 }
